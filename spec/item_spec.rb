3e2be265b290a36c84b6729959b3e45ee49dd6f1
Tanner Dale <tanner@tannerdale.com>
2021-08-11 10:18:53 -0500

    Add bidding test and method

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,4 +1,5 @@
 require './lib/item'
+require './lib/attendee'
 
 RSpec.describe Item do
   context 'initialize' do
@@ -10,6 +11,25 @@ RSpec.describe Item do
 
     it 'has attributes' do
       expect(item1.name).to eq('Chalkware Piggy Bank')
+      expect(item1.bids).to eq({})
+    end
+  end
+
+  context 'bid adding' do
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+
+    item1 = Item.new('Chalkware Piggy Bank')
+
+    it 'can add a bid' do
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+
+      expected = {
+        attendee2 => 20,
+        attendee1 => 22
+      }
+      expect(item1.bids).to eq(expected)
     end
   end
 end
