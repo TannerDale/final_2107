c24dd239b4d9dfbd10ac201dc10d079cb63c1db2
Tanner Dale <tanner@tannerdale.com>
2021-08-11 11:53:24 -0500

    Add can_buy? and buy tests and methods

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -5,4 +5,12 @@ class Attendee
     @name = params[:name]
     @budget = params[:budget].delete('$').to_i
   end
+
+  def can_buy?(bid)
+    @budget >= bid
+  end
+
+  def buy(amount)
+    @budget -= amount
+  end
 end
--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe Attendee do
       expect(attendee.budget).to eq(50)
     end
   end
+
+  context 'buying' do
+    attendee = Attendee.new(name: 'Megan', budget: '$50')
+
+    it 'can buy' do
+      expect(attendee.can_buy?(40)).to be(true)
+
+      attendee.buy(40)
+
+      expect(attendee.can_buy?(40)).to be(false)
+      expect(attendee.budget).to eq(10)
+    end
+  end
 end
